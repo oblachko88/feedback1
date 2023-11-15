47bfeeceda0e2ce70c6de18d40299d0cdccc6392
oblako88 <88oblachko@gmail.com>
2023-11-15 13:37:20 +0200

    second commit

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-  path("", views.review)
+  path("", views.review),
+  path("thank-you", views.thank_you)
 ]
