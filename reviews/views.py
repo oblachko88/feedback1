8f1f50082841bbfe4be1bd6eb10224c464b89fd3
oblako88 <88oblachko@gmail.com>
2023-11-20 10:33:43 +0200

    commit

--- reviews/views.py
+++ reviews/views.py
@@ -5,14 +5,6 @@ from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
-  # if request.method == 'POST':
-  #   entered_username = request.POST['username']
-  #   if entered_username == "" and len(entered_username) >= 100:
-  #     return render(request, "reviews/review.html", {
-  #       "has_error": True
-  #     })
-  #   print(entered_username)
-  #   return HttpResponseRedirect("/thank-you")
 
   form = ReviewForm()
 
