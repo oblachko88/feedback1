0b4cadeee71bcefedd707959f964a70056c97ad3
oblako88 <88oblachko@gmail.com>
2023-11-22 11:48:28 +0200

    models added

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  user_name = forms.CharField()
+  user_name = forms.CharField(label="Your name", max_length=100, error_messages={
+    "requiered": "Your name must not be empty!",
+    "max_lenght": "Please enter a shorter name!"
+  })
+  review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+  rating = forms.IntegerField(label="Your rating", min_value=0, max_value=5)
--- reviews/views.py
+++ reviews/views.py
@@ -5,8 +5,15 @@ from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
+  if request.method == 'POST':
+    form = ReviewForm(request.POST)
+    
+    if form.is_valid():
+      print(form.cleaned_data)
+      return HttpResponseRedirect("/thank-you")
 
-  form = ReviewForm()
+  else:
+    form = ReviewForm()
 
   return render(request, "reviews/review.html", {
     "form": form
