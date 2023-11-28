fa5df2dcecc7271b2435761d254015c1bca9a47e
oblako88 <88oblachko@gmail.com>
2023-11-28 15:17:40 +0200

    commit

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profiles", views.CreateProfileView.as_view())
+    path("profiles", views.CreateProfileView.as_view()),
+    path("profiles/list", views.ProfilesView.as_view())
 ]
